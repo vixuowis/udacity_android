c1be3fa21cc685bf6397209bde7dc54db6794caf
simon_xia <simon_xia@apple.com>
2017-02-08 16:49:54 +0800

    Finish project 0

--- app/src/main/java/com/example/simon/helloworld/MainActivity.java
+++ app/src/main/java/com/example/simon/helloworld/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.simon.helloworld;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void myToast(View v) {
+        Context context = getApplicationContext();
+        Button b = (Button)v;
+        String buttonText = b.getText().toString();
+        CharSequence text = "启动应用: " + buttonText;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
 }
+
+
